635bc1831c1bfc5d28f37ed07c49fe3b7e44cf58
gange <749918694@qq.com>
2024-02-26 22:36:23 +0800

    添加剩下的两个页面

--- src/utils/getFaqList.ts
+++ src/utils/getFaqList.ts
@@ -1,37 +1,141 @@
-const getFaqList = () => {
-  const faqs: Faq[] = [
-    {
-      question: "1. What is chronological age?",
-      answer:
-        "Chronological age is the amount of time that has passed from a person's birth to a given date. It is commonly quoted in years, months, weeks, days, etc?\n" +
-        "\n" +
-        "Chronological age is different to biological age because biological age takes into account for different biological and physiological development factors including genetics, lifestyle, nutrition and health conditions.",
-    },
-    {
-      question: "2. Why Use a Chronological Age Calculator?",
-      answer:
-        "Medical Purposes\n" +
-        "In the medical field, calculating a patient's actual age is crucial for diagnosis and treatment. The Chronological Age Calculator assists doctors in more accurately assessing a patient's health.\n" +
-        "\n" +
-        "Health Management\n" +
-        "Personal health and fitness plans are often tailored based on age. Using the Chronological Age Calculator ensures that plans and recommendations are based on accurate age data.\n" +
-        "\n" +
-        "Insurance Business\n" +
-        "Insurance companies typically base insurance premiums on the age of the insured. Accurately calculating the age of the insured is essential to ensure fair insurance rates.",
-    },
-    {
-      question: "3. How to calculate chronological age?",
-      answer:
-        "To calculate your chronological age you need to subtract your date of birth from the current date. However, this is easier said than done because different months have a different number of days. February, for example, has 28 days in a normal year and 29 in a leap year. You can get a rough idea of how old you are in days by calculating as follows:Age in days = (years × 365) + (months × 31) + days",
-    },
-    {
-      question: "4. Using our chronological age calculator",
-      answer:
-        "When you enter a date of birth into our age calculator, the function subtracts that date from the current one (or one you enter) in order to return the difference between the two dates. It then formats the result to display it in chronological order - meaning that it displays years, months and then days. As a handy feature, you're also able to find out statistics for the number of months & days, weeks and days individually.",
-    },
-  ];
-
-  return faqs;
+const getFaqList = index => {
+  if (index === "1") {
+    const faqs: Faq[] = [
+      {
+        question: "1. What is chronological age?",
+        answer:
+          "Chronological age is the amount of time that has passed from a person's birth to a given date. It is commonly quoted in years, months, weeks, days, etc?\n" +
+          "\n" +
+          "Chronological age is different to biological age because biological age takes into account for different biological and physiological development factors including genetics, lifestyle, nutrition and health conditions.",
+      },
+      {
+        question: "2. Why Use a Chronological Age Calculator?",
+        answer:
+          "Medical Purposes\n" +
+          "In the medical field, calculating a patient's actual age is crucial for diagnosis and treatment. The Chronological Age Calculator assists doctors in more accurately assessing a patient's health.\n" +
+          "\n" +
+          "Health Management\n" +
+          "Personal health and fitness plans are often tailored based on age. Using the Chronological Age Calculator ensures that plans and recommendations are based on accurate age data.\n" +
+          "\n" +
+          "Insurance Business\n" +
+          "Insurance companies typically base insurance premiums on the age of the insured. Accurately calculating the age of the insured is essential to ensure fair insurance rates.",
+      },
+      {
+        question: "3. How to calculate chronological age?",
+        answer:
+          "To calculate your chronological age you need to subtract your date of birth from the current date. However, this is easier said than done because different months have a different number of days. February, for example, has 28 days in a normal year and 29 in a leap year. You can get a rough idea of how old you are in days by calculating as follows:Age in days = (years × 365) + (months × 31) + days",
+      },
+      {
+        question: "4. Using our chronological age calculator",
+        answer:
+          "When you enter a date of birth into our age calculator, the function subtracts that date from the current one (or one you enter) in order to return the difference between the two dates. It then formats the result to display it in chronological order - meaning that it displays years, months and then days. As a handy feature, you're also able to find out statistics for the number of months & days, weeks and days individually.",
+      },
+    ];
+    return faqs;
+  } else if (index === "2") {
+    const faqs: Faq[] = [
+      {
+        question: "1. What is Pearson Age Calculator?",
+        answer:
+          "Welcome to our cutting-edge tool, the Pearson Age Calculator. In this dedicated page, we will explore the functionality and incredible benefits of this feature, designed to provide you with precise age calculations. Whether you're a healthcare professional, an educator, or someone curious about accurate age determination, our Pearson Age Calculator is here to empower you.\n" +
+          "\n" +
+          "The Pearson Age Calculator is an advanced tool meticulously engineered to calculate an individual's age with the utmost precision. It takes into consideration the person's date of birth and the current date, delivering a detailed age calculation in years, months, and days.",
+      },
+      {
+        question: "2. Pearson Age Calculator Key Features?",
+        answer:
+          "Unparalleled Precision\n" +
+          "What sets our Pearson Age Calculator apart is its unmatched precision. It meticulously accounts for leap years and provides you with reliable and exact age calculations every time you use it.\n" +
+          "\n" +
+          "Ease of Use\n" +
+          "We've designed the calculator with user-friendliness as a top priority. Its intuitive interface ensures that individuals of all technical backgrounds can effortlessly harness its power.\n" +
+          "\n" +
+          "Versatility\n" +
+          "This feature's versatility is one of its standout qualities. It finds applications across various industries and is indispensable for a multitude of purposes.",
+      },
+      {
+        question: "3. How to Utilize the Pearson Age Calculator?",
+        answer:
+          "Using the Pearson Age Calculator is a straightforward process:\n" +
+          "\n" +
+          "1.Input the individual's date of birth into the designated field.\n" +
+          "\n" +
+          "2.Enter the current date.\n" +
+          "\n" +
+          '3.Click the "Calculate" button.\n' +
+          "\n" +
+          "Within seconds, you will receive a precise and comprehensive chronological age report that can be used for diverse requirements.",
+      },
+      {
+        question: "4. Applications",
+        answer:
+          "The Pearson Age Calculator finds applications across a wide spectrum of fields:\n" +
+          "\n" +
+          "Medical Realm: Healthcare professionals rely on it for accurate patient assessments and age-based medical decisions.\n" +
+          "\n" +
+          "Educational Sector: Educators and researchers utilize it for academic research and data analysis.\n" +
+          "\n" +
+          "Insurance Industry: Insurance companies employ it to determine policy rates based on the insured's age.",
+      },
+      {
+        question: "5. Why Choose Our Pearson Age Calculator?",
+        answer:
+          "Unwavering Reliability: Our calculator is renowned for its unwavering reliability and the accuracy of its results, ensuring your age calculations are always precise.\n" +
+          "\n" +
+          "User-Centric Design: We've prioritized a user-centric approach in designing this tool, making it accessible and hassle-free for everyone.\n" +
+          "\n" +
+          "Endless Versatility: Its adaptability to different industries and purposes makes it an indispensable asset",
+      },
+    ];
+    return faqs;
+  } else if (index === "3") {
+    const faqs: Faq[] = [
+      {
+        question: "1. What is age chronological calculator?",
+        answer:
+          "In today's digital age, having access to accurate and efficient tools is essential. Among the many valuable tools available, our Age Chronological Calculator stands out as a versatile and indispensable resource. In this article, we will take a closer look at the capabilities and benefits of this feature.\n" +
+          "\n" +
+          "Our Age Chronological Calculator is a feature designed to help you calculate a person's chronological age quickly and accurately. It takes two crucial pieces of information into account: the individual's date of birth and the current date. By simply inputting these dates, the calculator generates a precise age calculation, presenting results in years, months, and days.",
+      },
+      {
+        question: "2. Age Chronological Calculator Key Features",
+        answer:
+          "Precision\n" +
+          "One of the standout features of our Age Chronological Calculator is its precision. It factors in leap years and offers reliable results, making it a trusted tool in various fields.\n" +
+          "\n" +
+          "Ease of Use\n" +
+          "We've designed this feature with user-friendliness in mind. It's intuitive and straightforward, ensuring that users of all levels of technical expertise can utilize it effortlessly.\n" +
+          "\n" +
+          "Versatility\n" +
+          "The calculator has a wide range of applications, making it valuable in several industries and for various purposes. Its versatility is what sets it apart from traditional age calculators.",
+      },
+      {
+        question: "3. How to Use the Age Chronological Calculator?",
+        answer:
+          'Using our Age Chronological Calculator is a straightforward process: 1、Input the individual\'s date of birth into the designated field. 2、Enter the current date. 3、Click the "Calculate" button. Within moments, you will receive an accurate and detailed chronological age report that can be used for your specific needs.',
+      },
+      {
+        question: "4. Applications",
+        answer:
+          "The Age Chronological Calculator finds applications in numerous fields:\n" +
+          "\n" +
+          "Medical Field: Healthcare professionals rely on it for patient assessments and accurate age-based diagnoses.\n" +
+          "\n" +
+          "Insurance Industry: Insurance companies use it to determine premium rates based on the insured's age.\n" +
+          "\n" +
+          "Human Resources: HR departments depend on it for workforce planning, retirement calculations, and more.\n" +
+          "\n" +
+          "Why Choose Our Age Chronological Calculator?\n" +
+          "\n" +
+          "Reliability: The calculator is renowned for its accuracy and reliability, ensuring that you get precise results every time you use it.\n" +
+          "\n" +
+          "User-Friendly: We've designed it with ease of use in mind, making it accessible to everyone.\n" +
+          "\n" +
+          "Versatility: Its adaptability to different industries and purposes makes it a valuable asset.",
+      },
+    ];
+    return faqs;
+  }
 };
 
 export interface Faq {
